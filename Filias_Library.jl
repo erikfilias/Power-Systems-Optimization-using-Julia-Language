530401d3a533cdab3f8b495b886623937cf9e024
Erik Alvarez <erikfilias@gmail.com>
2019-02-22 17:34:22 -0300

    Update Master

--- Filias_Library.jl
+++
@@ -1,8 +0,0 @@
-function squareroot(x)
-	z = x # Initial starting point for Newton’s method
-	while abs(z*z - x) > 1e-13
-	z = z - (z*z-x)/(2z)
-	end
-	return z
-end
-JuMP.register(m, :squareroot, 1, squareroot, autodiff=true)
